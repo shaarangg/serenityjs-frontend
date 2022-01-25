2b69aba89a903fb6f70e4a5bc48abafcc4788aa9
Shaarang Singh <shaarangg.singh@gmail.com>
2022-01-25 12:55:38 +0530

    [WIP]: Navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/Navbar.scss";
 import { GoThreeBars } from "react-icons/go";
 function Navbar() {
 	const [showLinks, setShowLinks] = useState(false);
+	const linkContainerRef = useRef(null);
+	const linksRef = useRef(null);
+	// useEffect(() => {
+	// 	if (showLinks) {
+	// 		const linkHeight = linksRef.current.getBoundingClientRect().height;
+	// 		console.log(linkHeight);
+	// 		linkContainerRef.current.style.height = `${linkHeight}px`;
+	// 	} else {
+	// 		linkContainerRef.current.style.height = "0px";
+	// 	}
+	// }, [showLinks]);
 	return (
-		<nav className="nav">
-			<div className="nav-head">
-				<p className="logo">Serenity.js</p>
-				<button className="nav-toggle">
-					<GoThreeBars />
-				</button>
-			</div>
-			<div className="nav-center">
-				<a href="">Home</a>
-				<a href="">Documentation</a>
-				<a href="">Contact Us</a>
-			</div>
-			<div className="nav-end">
-				<a href="">Test API</a>
-			</div>
-		</nav>
+		<div className="container">
+			<nav className="nav">
+				<div className="nav-head">
+					<p className="logo">Serenity.js</p>
+					<button
+						className="nav-toggle"
+						onClick={() => {
+							setShowLinks(!showLinks);
+						}}
+					>
+						<GoThreeBars />
+					</button>
+				</div>
+				<div className="nav-center">
+					<a href="">Home</a>
+					<a href="">Documentation</a>
+					<a href="">Contact Us</a>
+				</div>
+				<div className="nav-end">
+					<a href="">Test API</a>
+				</div>
+			</nav>
+		</div>
 	);
 }
 
