c2a7f0532232f6f3ddbea2faa0da1f889e303128
Shaarang Singh <shaarangg.singh@gmail.com>
2022-01-30 21:17:12 +0530

    feat: API pager

--- src/pages/Api/Api.jsx
+++ src/pages/Api/Api.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import styles from "./Api.module.scss";
 function Api() {
-	return <div>Api</div>;
+	return (
+		<div className={styles.container}>
+			<textarea name="" id="" placeholder="Enter text"></textarea>
+			<div className={styles.btnContainer}>
+				<button type="submit">Submit</button>
+				<p>Class: </p>
+			</div>
+		</div>
+	);
 }
 
 export default Api;
