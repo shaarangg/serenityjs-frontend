7997d4f95c72dac2388f0ae8a80dc01f07687298
Shaarang Singh <shaarangg.singh@gmail.com>
2022-01-31 16:57:16 +0530

    feat: API route work now

--- src/pages/Api/Api.jsx
+++ src/pages/Api/Api.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef, useState } from "react";
 import styles from "./Api.module.scss";
 function Api() {
+	const textRef = useRef(null);
+	const [status, setStatus] = useState("");
+	const [disabled, setDisabled] = useState(false);
+	const handleSubmit = async () => {
+		setDisabled(true);
+		const res = await axios.post("https://api.myserenity.live/predict", {
+			text: textRef.current.value,
+		});
+		setStatus(res.data);
+		setDisabled(false);
+	};
 	return (
 		<div className={styles.container}>
-			<textarea name="" id="" placeholder="Enter text"></textarea>
+			<textarea name="" id="" placeholder="Enter text" ref={textRef}></textarea>
 			<div className={styles.btnContainer}>
-				<button type="submit">Submit</button>
-				<p>Class: </p>
+				<button type="submit" disabled={disabled} onClick={handleSubmit}>
+					Submit
+				</button>
+				<p>Class: {status}</p>
 			</div>
 		</div>
 	);
--- src/pages/Docs/Docs.jsx
+++ src/pages/Docs/Docs.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import styles from "./Docs.module.scss";
+import Sidebar from "../../components/Sidebar/Sidebar";
 function Docs() {
-	return <div>Docs</div>;
+	return (
+		<div className={styles.container}>
+			<Sidebar />
+		</div>
+	);
 }
 
 export default Docs;
