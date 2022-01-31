eb06c24cc0bdb635311042f8c11dc031527cd174
Shaarang Singh <shaarangg.singh@gmail.com>
2022-01-31 17:50:27 +0530

    feat: Changes in Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,15 @@ import styles from "./Home.module.scss";
 function Home() {
 	return (
 		<div className={styles.container}>
-			<h2 className={styles.title}>The React Framework for Production</h2>
-			<p className={styles.content}>
-				With our project we aim to tackle this problem in online social media platforms where we use
-				Natural Language Processing to detect and correct racism as well as all forms of
-				microaggression and foul criticism. Our goal is to generate an online platform that has the
-				ability to detect and correct such content to make the reduce the forms of online abuse and
-				racism.
-			</p>
+			<div className={styles.head}>
+				<div className={styles.title}>The React Framework for Production</div>
+				<p className={styles.content}>
+					Sernity.js aims to tackle the problem in social media platforms where it uses the power of
+					Natural Language Processing to detect and correct racism as well as all forms of
+					microaggression and foul criticism. It gives you the best developer experience by giving
+					one call detection of hate speech in the input text.
+				</p>
+			</div>
 			<div className={styles.btnContainer}>
 				<button type="button" className={styles.Docsbtn}>
 					Start Learning
