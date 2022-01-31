2a04faf9e05ea57257e3edc61ed61c11ac743df7
Shaarang Singh <shaarangg.singh@gmail.com>
2022-01-31 19:18:50 +0530

    feat: Home buttons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import styles from "./Home.module.scss";
+import { Link } from "react-router-dom";
 function Home() {
 	return (
 		<div className={styles.container}>
-			<div className={styles.head}>
-				<div className={styles.title}>Authentic, Distinctive, Convenient. </div>
-				<p className={styles.content}>
-					Sernity.js aims to tackle the problem in social media platforms where it uses the power of
-					Natural Language Processing to detect and correct racism as well as all forms of
-					microaggression and foul criticism. It gives you the best developer experience by giving
-					one call detection of hate speech in the input text.
-				</p>
-			</div>
+			<div className={styles.title}>Authentic, Distinctive, Convenient. </div>
+			<p className={styles.content}>
+				Sernity.js aims to tackle the problem in social media platforms where it uses the power of
+				Natural Language Processing to detect and correct racism as well as all forms of
+				microaggression and foul criticism. It gives you the best developer experience by giving one
+				call detection of hate speech in the input text.
+			</p>
 			<div className={styles.btnContainer}>
 				<button type="button" className={styles.Docsbtn}>
-					Start Learning
+					<Link to="/docs">Start Learning</Link>
 				</button>
 				<button type="button" className={styles.Apibtn}>
-					Test API
+					<Link to="/api">Test API</Link>
 				</button>
 			</div>
 		</div>
