93464e757a74bbb753ff138aa00cdb1321e2ce10
Shaarang Singh <shaarangg.singh@gmail.com>
2022-02-01 22:15:16 +0530

    feat: Sidebar dropdown

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,30 +1,43 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowDown } from "react-icons/md";
+import styles from "./Sidebar.module.scss";
 export default function Sidebar() {
+	const [showBar, setShowBar] = useState(false);
 	return (
-		<ul>
-			Documentation
-			<li>
-				<a href="">Getting Started</a>
-			</li>
-			<li>
-				<a href="">Prerequisites</a>
-			</li>
-			<li>
-				<a href="">About Serinity.js</a>
-			</li>
-			<li>
-				<a href="">Built With</a>
-			</li>
-			<li>
-				<a href="">API Routes</a>
-			</li>
-			<li>
-				<a href="">Setup</a>
-			</li>
-			<li>
-				<a href="">Testing</a>
-			</li>
-		</ul>
+		<div className={styles.container}>
+			<button
+				className={styles.sideToggle}
+				onClick={() => {
+					setShowBar(!showBar);
+				}}
+			>
+				{showBar ? <MdOutlineKeyboardArrowDown /> : <MdOutlineKeyboardArrowRight />}
+				<p>Menu</p>
+			</button>
+			<ul className={styles.list}>
+				<p>Documentation</p>
+				<li>
+					<a href="">Getting Started</a>
+				</li>
+				<li>
+					<a href="">Prerequisites</a>
+				</li>
+				<li>
+					<a href="">About Serinity.js</a>
+				</li>
+				<li>
+					<a href="">Built With</a>
+				</li>
+				<li>
+					<a href="">API Routes</a>
+				</li>
+				<li>
+					<a href="">Setup</a>
+				</li>
+				<li>
+					<a href="">Testing</a>
+				</li>
+			</ul>
+		</div>
 	);
 }
