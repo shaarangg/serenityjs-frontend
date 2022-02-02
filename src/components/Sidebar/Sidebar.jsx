13547dba1404d9b3ccf14599f076c70c4d802e16
Shaarang Singh <shaarangg.singh@gmail.com>
2022-02-02 10:52:38 +0530

    feat: Updated the favicon

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowDown } from "react-icons/md";
 import styles from "./Sidebar.module.scss";
 export default function Sidebar() {
-	const [showBar, setShowBar] = useState(false);
+	const [showBar, setShowBar] = useState(true);
+	const listRef = useRef(null);
+
+	useEffect(() => {
+		if (showBar) {
+			listRef.current.style.height = "100vh";
+		} else {
+			listRef.current.style.height = "0px";
+			console.log(listRef);
+		}
+	});
+
 	return (
 		<div className={styles.container}>
 			<button
@@ -14,27 +25,34 @@ export default function Sidebar() {
 				{showBar ? <MdOutlineKeyboardArrowDown /> : <MdOutlineKeyboardArrowRight />}
 				<p>Menu</p>
 			</button>
-			<ul className={styles.list}>
+			<ul className={styles.list} ref={listRef}>
 				<p>Documentation</p>
 				<li>
+					<span></span>
 					<a href="">Getting Started</a>
 				</li>
 				<li>
+					<span></span>
 					<a href="">Prerequisites</a>
 				</li>
 				<li>
+					<span></span>
 					<a href="">About Serinity.js</a>
 				</li>
 				<li>
+					<span></span>
 					<a href="">Built With</a>
 				</li>
 				<li>
+					<span></span>
 					<a href="">API Routes</a>
 				</li>
 				<li>
+					<span></span>
 					<a href="">Setup</a>
 				</li>
 				<li>
+					<span></span>
 					<a href="">Testing</a>
 				</li>
 			</ul>
